8117c23084d5aeb72c3116439d293adb6baba8e0
simon9141 <simonotieno9141@gmail.com>
2023-06-23 06:18:48 -0400

    done 3 tasks

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,20 +12,46 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const promise = new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            resolve(books),
+            6000
+        })
+    })
+    promise.then((result)=>{
+        return res.status(200).json({ books: result });
+    })
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const book = books[req.params.isbn]
+
+    if(book){
+        return res.status(200).json({book})
+    }
+    else{
+        return res.status(404).json({message:"book not found"})
+    }
+     
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let ans = []
+    for(const [key, values] of Object.entries(books)){
+        const book = Object.entries(values);
+        for(let i = 0; i < book.length ; i++){
+            if(book[i][0] == 'author' && book[i][1] == req.params.author){
+                ans.push(books[key]);
+            }
+        }
+    }
+    if(ans.length == 0){
+        return res.status(404).json({message: "Author not found"});
+    }
+    res.send(ans);
+ 
 });
 
 // Get all books based on title
